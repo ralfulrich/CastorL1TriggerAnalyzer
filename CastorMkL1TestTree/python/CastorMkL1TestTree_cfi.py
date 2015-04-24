b3bf5ab61c8e539580f3131391cbab570abde3d8
Clemens <Hauke.Woehrmann@kit.edu>
2015-04-24 14:49:52 +0200

    update parameter set

--- CastorMkL1TestTree/python/CastorMkL1TestTree_cfi.py
+++ CastorMkL1TestTree/python/CastorMkL1TestTree_cfi.py
@@ -1,4 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-demo = cms.EDAnalyzer('CastorMkL1TestTree'
+castormkl1testtree = cms.EDAnalyzer('CastorMkL1TestTree',
+    PileUpInfo       = cms.InputTag('addPileupInfo','','HLT'),
+    GenPartColl      = cms.InputTag('genParticles'),
+    GenJetColl       = cms.InputTag('ak5GenJets'),
+    CaloTowerColl    = cms.InputTag('towerMaker'),
+    HadFwdRecHitColl = cms.InputTag('hfreco'),
+    CasRecHitColl    = cms.InputTag('castorreco'),
+    BasicJetColl     = cms.InputTag('ak5CastorJets'),
+    CastorJetID      = cms.InputTag('ak5CastorJetID'),
+    TrigResults      = cms.InputTag('TriggerResults','','HLT2'),
+    VertexColl       = cms.InputTag('offlinePrimaryVertices')
 )
