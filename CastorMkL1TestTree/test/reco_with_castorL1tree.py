2eb18a1b267e234c6cf3204916878a7df64136fe
Clemens <Hauke.Woehrmann@kit.edu>
2015-04-24 15:03:03 +0200

    getting parameter now via import

--- CastorMkL1TestTree/test/reco_with_castorL1tree.py
+++ CastorMkL1TestTree/test/reco_with_castorL1tree.py
@@ -89,18 +89,8 @@ process.configurationMetadata = cms.untracked.PSet(
 
 #################################################################################################################################
 #################################################################################################################################
-process.castormkl1testtree = cms.EDAnalyzer('CastorMkL1TestTree',
-    PileUpInfo       = cms.InputTag('addPileupInfo','','HLT'),
-    GenPartColl      = cms.InputTag('genParticles'),
-    GenJetColl       = cms.InputTag('ak5GenJets'),
-    CaloTowerColl    = cms.InputTag('towerMaker'),
-    HadFwdRecHitColl = cms.InputTag('hfreco'),
-    CasRecHitColl    = cms.InputTag('castorreco'),
-    BasicJetColl     = cms.InputTag('ak5CastorJets'),
-    CastorJetID      = cms.InputTag('ak5CastorJetID'),
-    TrigResults      = cms.InputTag('TriggerResults','','HLT2'),
-    VertexColl       = cms.InputTag('offlinePrimaryVertices')
-)
+import CastorL1TriggerAnalyzer.CastorMkL1TestTree.CastorMkL1TestTree_cfi
+process.castormkl1testtree = CastorL1TriggerAnalyzer.CastorMkL1TestTree.CastorMkL1TestTree_cfi.castormkl1testtree.clone()
 #################################################################################################################################
 
 
